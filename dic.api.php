b6951427930ac95bdbc3f08465bd8b605a9e5034
Jan Schumann <jan.schumann@bauermedia.com>
2014-03-21 16:06:51 +0100

    reamed bundle hook. it now also gathers information about autoloading.

--- dic.api.php
+++ dic.api.php
@@ -1,10 +1,19 @@
 <?php
 
 /**
- * Provide the dic with bundles
+ * Provide the dic with information about bundles
+ *
+ * That is the bundles classes and their autoloading paths
  *
  * @return array
  */
-function hook_register_bundle() {
-  return array("\\Drupal\\Dic\\Bundle\\DicBundle\\DicBundle");
+function hook_dic_bundle_info() {
+  return array(
+    'bundles' => array(
+      "\\Drupal\\Dic\\Bundle\\DicBundle\\DicBundle"
+    ),
+    'autoload' => array(
+      '\\Drupal\\Dic\\' => array(DRUPAL_ROOT . '/' . drupal_get_path('module', 'dic') . '/src'),
+    )
+  );
 }
