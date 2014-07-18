7af9936f949d1fa6215c901e301bb68504523dd4
Jan Schumann <jan.schumann@bauermedia.com>
2014-07-18 10:43:14 +0200

    removed configuration parsing as dic bundle does not contain any configuration so far

--- src/Drupal/Dic/Bundle/DicBundle/DependencyInjection/DicExtension.php
+++ src/Drupal/Dic/Bundle/DicBundle/DependencyInjection/DicExtension.php
@@ -29,9 +29,6 @@ class DicExtension extends Extension
     $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 
     $loader->load('services.xml');
-
-    $configuration = $this->getConfiguration($configs, $container);
-    $config = $this->processConfiguration($configuration, $configs);
   }
 
   /**
